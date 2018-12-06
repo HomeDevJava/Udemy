9304c15f096544e9ef33ca8efd2a234f1a5c6db6
50098250 <50098250@SRTPC50098250.GEPP.PVT>
2018-12-06 12:01:50 -0600

    se implementa la interfaz CommandLineRunner para que se cree y borre en automatico el folder upload

--- CursoSpringMVC/src/main/java/com/cursospring/app/CursoSpringMvcApplication.java
+++ CursoSpringMVC/src/main/java/com/cursospring/app/CursoSpringMvcApplication.java
@@ -1,12 +1,25 @@
 package com.cursospring.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursospring.app.models.service.IUploadFileService;
+
 @SpringBootApplication
-public class CursoSpringMvcApplication {
+public class CursoSpringMvcApplication implements CommandLineRunner{
+	
+	@Autowired IUploadFileService updloadService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringMvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		updloadService.deleteAll();
+		updloadService.init();
 	}
 }
