033606a97b08c7dc17c0c0c47513ead9278c2388
50098250 <50098250@SRTPC50098250.GEPP.PVT>
2018-12-14 17:30:22 -0600

    Implementando el metodo eliminarFactura, este metodo elimina la factura y las lineas de los items o productos atraves de la anotacion cascade=CascadeType.ALL

--- CursoSpringMVC/src/main/java/com/cursospring/app/controllers/FacturaController.java
+++ CursoSpringMVC/src/main/java/com/cursospring/app/controllers/FacturaController.java
@@ -108,5 +108,18 @@ public class FacturaController {
 		return "redirect:/ver/" + factura.getCliente().getId();
 
 	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable Long id, RedirectAttributes flash) {
+		Factura factura= clienteService.findFacturaById(id);
+		if(factura!=null) {
+			clienteService.deleteFactura(id);
+			flash.addFlashAttribute("success", "Factura eliminada con exito");
+			return "redirect:/ver/"+factura.getCliente().getId();
+		}
+		
+		flash.addFlashAttribute("error", "Factura NO existe en la BD, no se pudo eliminar");
+		return "redirect:/listar";
+	}
 
 }
