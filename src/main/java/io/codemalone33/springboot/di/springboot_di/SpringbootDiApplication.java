f3b3b726ff92a7ab0d7453a961ac918832a4e96c
Jisashi Nakamura <jisashi82@gmail.com>
2024-08-01 12:30:59 -0600

    SEC3_44:SpringBoot3-Inyeccion de Dependencias IoC

---
+++ src/main/java/io/codemalone33/springboot/di/springboot_di/SpringbootDiApplication.java
@@ -0,0 +1,13 @@
+package io.codemalone33.springboot.di.springboot_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiApplication.class, args);
+	}
+
+}
