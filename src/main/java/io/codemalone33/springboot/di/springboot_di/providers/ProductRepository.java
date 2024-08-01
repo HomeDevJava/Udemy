a4d7967c92e3d2f924d733df0fca2ef777e08045
Jisashi Nakamura <jisashi82@gmail.com>
2024-08-01 12:40:47 -0600

    SEC3_45:Creando la clase Model y Repository de Product

---
+++ src/main/java/io/codemalone33/springboot/di/springboot_di/providers/ProductRepository.java
@@ -0,0 +1,28 @@
+package io.codemalone33.springboot.di.springboot_di.providers;
+
+import java.util.List;
+
+import io.codemalone33.springboot.di.springboot_di.models.Product;
+
+public class ProductRepository {
+
+    List<Product> data;
+
+    public ProductRepository() {
+        this.data= List.of(
+                new Product(1L, "Memoria Corsair 32GB", 659L),
+                new Product(2L, "Cpu Intel Core i7 11200F", 2000L),
+                new Product(3L, "MotherBoard MSI B450", 1500L),
+                new Product(4L, "Gabinete Corsair", 400L),
+                new Product(5L, "Placa de Video MSI", 1000L),
+                new Product(6L, "Disco SSD 1TB", 800L)
+        );
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+    
+
+}
