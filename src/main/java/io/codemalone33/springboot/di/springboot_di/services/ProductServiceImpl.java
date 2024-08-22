052e91980d66ed2bf5f5ab0e1bfecba623006f8a
Jisashi Nakamura <jisashi82@gmail.com>
2024-08-21 19:00:53 -0600

    SEC3_60-61:Leer el pricetax desde un archivo properties

--- src/main/java/io/codemalone33/springboot/di/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/io/codemalone33/springboot/di/springboot_di/services/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.codemalone33.springboot.di.springboot_di.models.Product;
@@ -17,15 +18,18 @@ public class ProductServiceImpl  implements ProductService {
     @Qualifier("productRepositoryImpl")
     private ProductoRepository productRepository;
 
+    @Value("${config.product.price.tax}")
+    private Double tax;
+
     @Override
     public List<Product> findAll() {
         return productRepository.findAll().stream().map(p ->{
-            Double priceTax= p.getPrice() * 1.21;
-            p.setPrice(priceTax.longValue());
-            return p;
-            // Product newProd=(Product) p.clone();
-            // newProd.setPrice(priceTax.longValue());
-            // return newProd ;
+            Double priceTax= p.getPrice() * tax;
+            /* p.setPrice(priceTax.longValue());
+            return p; */
+            Product newProd=(Product) p.clone();
+            newProd.setPrice(priceTax.longValue());
+            return newProd ;
            
             //se regresa un nuevo objeto para que no se modifique el objeto original
             //return new Product(p.getId(), p.getName(), priceTax.longValue());
