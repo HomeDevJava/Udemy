97c740b6eedd9c24964b51b3809356e2aba947ab
Jisashi Nakamura <jisashi82@gmail.com>
2024-09-24 11:02:49 -0600

    SEC5_81:Añadiendo el metodo Handler para el manejo de la excepcion

--- springboot-error/src/main/java/io/codemalone33/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ springboot-error/src/main/java/io/codemalone33/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,15 +1,25 @@
 package io.codemalone33.springboot.error.springboot_error.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.codemalone33.springboot.error.springboot_error.models.Error;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
 
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZero(Exception e) {
-        return ResponseEntity.internalServerError().body("Division by zero");
+    public ResponseEntity<Error> divisionByZero(Exception e) {
+        Error error = new Error();
+        error.setMessage(e.getMessage());
+        error.setError("Division by zero");
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.setDate(new Date());
+        return ResponseEntity.internalServerError().body(error);
     }
 
 }
