f0573906f267ee51c7fe58a93d68d26408f28c6c
Jisashi Nakamura <jisashi82@gmail.com>
2024-10-10 11:07:54 -0600

    SEC6_90-91:Introduccion a los Interceptores Http

---
+++ springboot-interceptors/src/test/java/io/codemalone/springboot/app/interceptors/springboot_interceptors/SpringbootInterceptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codemalone.springboot.app.interceptors.springboot_interceptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
