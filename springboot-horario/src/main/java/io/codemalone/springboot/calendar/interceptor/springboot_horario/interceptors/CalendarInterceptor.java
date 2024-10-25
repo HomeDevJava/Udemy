13c61b141c2f73c4b2256c5fa8a876a0a41e17e6
Jisashi Nakamura <jisashi82@gmail.com>
2024-10-24 19:23:11 -0600

    SEC7_100:Implementando  cerrado en el interceptor

--- springboot-horario/src/main/java/io/codemalone/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
+++ springboot-horario/src/main/java/io/codemalone/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
@@ -1,12 +1,16 @@
 package io.codemalone.springboot.calendar.interceptor.springboot_horario.interceptors;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -37,12 +41,30 @@ public class CalendarInterceptor implements HandlerInterceptor {
             request.setAttribute("message", message.toString());
             return true;
         } else {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> body = new HashMap<>();
+
+            StringBuilder message= new StringBuilder("Cerrado, fuera del horario de atencion al cliente");
+            message.append(", visitenos desde las ")
+                    .append(open)
+                    .append(" hrs, hasta las ")
+                    .append(close)
+                    .append("vhoras \n");
             
+            body.put("message", message.toString());
+
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(mapper.writeValueAsString(body));
+
+            //mapper.writeValue(response.getWriter(), body);
             return false;
         }
 
     }
 
+
+    
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
