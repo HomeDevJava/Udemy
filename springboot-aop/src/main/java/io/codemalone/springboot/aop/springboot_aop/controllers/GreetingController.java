0e7c19709151cfde722ce8bd36ede1d654596679
Jisashi Nakamura <jisashi82@gmail.com>
2024-10-29 21:34:37 -0600

    SEC8_105:añadiendo el componente Service

--- springboot-aop/src/main/java/io/codemalone/springboot/aop/springboot_aop/controllers/GreetingController.java
+++ springboot-aop/src/main/java/io/codemalone/springboot/aop/springboot_aop/controllers/GreetingController.java
@@ -2,19 +2,25 @@ package io.codemalone.springboot.aop.springboot_aop.controllers;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.codemalone.springboot.aop.springboot_aop.services.IGreeting;
 
 
 @RestController
 @RequestMapping("/api")
 public class GreetingController {
 
+    @Autowired
+    private IGreeting greetingService;
+
     @GetMapping("/greeting")
     public ResponseEntity<?> greeting() {
-        return ResponseEntity.ok(Collections.singletonMap("greeting", "hello"));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Jisashi", "Hello, What's up ")));
     }
     
 
---
+++ springboot-aop/src/main/java/io/codemalone/springboot/aop/springboot_aop/services/IGreeting.java
@@ -0,0 +1,7 @@
+package io.codemalone.springboot.aop.springboot_aop.services;
+
+public interface IGreeting {
+
+    String sayHello(String person, String phrase);
+
+}
