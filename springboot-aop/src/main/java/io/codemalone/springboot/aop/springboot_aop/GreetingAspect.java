c1f4ce1378574ec147f1becc81b910407e604ff9
Jisashi Nakamura <jisashi82@gmail.com>
2024-10-30 22:22:44 -0600

    SEC8_107:Advice After y expresiones regulares o Patrones en el Pointcut

--- springboot-aop/src/main/java/io/codemalone/springboot/aop/springboot_aop/GreetingAspect.java
+++ springboot-aop/src/main/java/io/codemalone/springboot/aop/springboot_aop/GreetingAspect.java
@@ -1,7 +1,10 @@
 package io.codemalone.springboot.aop.springboot_aop;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -21,11 +24,12 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(GreetingAspect.class);
 
-    @Before("execution(* io.codemalone.springboot.aop.springboot_aop.services.IGreeting.sayHello(..))")
+    @Before("execution(* io.codemalone.springboot.aop.springboot_aop.services.*.*(..))")
     public void beforeGreeting(JoinPoint joinPoint) {
         
-        logger.info("before greeting");
+        logger.info("before greeting-------------------");
         String method= joinPoint.getSignature().getName();
+        logger.info("Class invoked: " + joinPoint.getSignature().getDeclaringTypeName());
         logger.info("Method invoked: " + method);
 
         //1 forma de obtener los argumentos
@@ -33,13 +37,22 @@ public class GreetingAspect {
         String argsString = Stream.of(args).map(Object::toString).collect(Collectors.joining(", "));
         logger.info("Arguments passed: " + args); */
         
-        //2a forma de obtener los argumentos utilizando stream y Arrays
-       /*  String argumentos= Arrays.stream(joinPoint.getArgs()).map(Object::toString).collect(Collectors.joining(", "));
-        logger.info("Arguments passed: " + argumentos); */
         
         //forma sencilla de obtener los argumentos
+        logger.info("Method invoked: " + joinPoint.getSignature().getName());
         String argsString = Arrays.toString(joinPoint.getArgs());
         logger.info("Arguments passed: " + argsString);
+    }
+    
+    @After("execution(* io.codemalone.springboot.aop.springboot_aop.services.IGreeting.*(..))")
+    public void afterGreeting(JoinPoint joinPoint) {
+        logger.info("after greeting-----------------------");
+        logger.info("Class invoked: " + joinPoint.getSignature().getDeclaringTypeName());
+        
+        
+        //2a forma de obtener los argumentos utilizando stream y Arrays
+        String argumentos= Arrays.stream(joinPoint.getArgs()).map(Object::toString).collect(Collectors.joining(", "));
+        logger.info("Arguments passed: " + argumentos);
     }
 
 }
