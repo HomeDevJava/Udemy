02631eea1d3607bc845894dc8bd9b38fc8ff8cfd
Jisashi Nakamura <jisashi82@gmail.com>
2024-11-07 18:15:23 -0600

    SEC9_114-116:Creando proyecto JPA e Hibernate

---
+++ srpingboot-jpa/src/test/java/io/codemalone/srpingboot_jpa/SrpingbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codemalone.srpingboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
