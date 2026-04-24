87e520dbbb03642c93044d90caca5c7a4cf06958
Abel Jisashi Nakamura <jisashi82@gmail.com>
2026-04-24 15:59:03 -0600

    SEC11_154:La relación ManyToOne

---
+++ spb_jpa_hibernate/src/main/java/io/codemalone/curso/springboot/jpa/spb_jpa_hibernate/entities/Invoice.java
@@ -0,0 +1,72 @@
+package io.codemalone.curso.springboot.jpa.spb_jpa_hibernate.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Long amount;
+
+    @ManyToOne
+    private Client client;
+
+
+    public Invoice(){}
+
+    public Invoice(String description, Long amount){
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public String toString() {
+        return "{id=" + id + ", description=" + description + ", amount=" + amount + ", client=" + client + "}";
+    }
+
+
+
+
+}
