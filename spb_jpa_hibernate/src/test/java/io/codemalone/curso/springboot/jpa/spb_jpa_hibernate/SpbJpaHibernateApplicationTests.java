ce64df5fcc1712a2c1fc234ed3b673132d8be960
Abel Jisashi Nakamura <jisashi82@gmail.com>
2026-04-24 14:17:36 -0600

    SEC11_153:JAP e Hibernate-Asociaciones

---
+++ spb_jpa_hibernate/src/test/java/io/codemalone/curso/springboot/jpa/spb_jpa_hibernate/SpbJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codemalone.curso.springboot.jpa.spb_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
